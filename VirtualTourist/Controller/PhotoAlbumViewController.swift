fefc7b9b86c921415031155452213ce0e8e7b13c
mvirgo <mvirgs44@gmail.com>
2020-06-27 18:06:22 -0700

    feat: successfully query flickr api for location photos

--- VirtualTourist/Controller/PhotoAlbumViewController.swift
+++ VirtualTourist/Controller/PhotoAlbumViewController.swift
@@ -14,13 +14,26 @@ class PhotoAlbumViewController: UIViewController {
     // MARK: Other Variables
     var dataController: DataController!
     var selectedCoordinate: CLLocationCoordinate2D!
+    // TODO: Switch below to actually store images
+    var images: LocationAlbum!
     
     // MARK: View Functions
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        print(selectedCoordinate)
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        // Get photos at the current location
+        APIClient.getPhotosByLocations(latitude: selectedCoordinate.latitude, longitude: selectedCoordinate.longitude, completion: handlePhotoData(response:error:))
+        // TODO: Go load the images themselves
     }
-
-
+    
+    // MARK: Completion Handlers
+    func handlePhotoData(response: LocationAlbum?, error: Error?) {
+        if let _ = error {
+            let alertVC = UIAlertController(title: "Download Failed", message: "Failed to download location photos.", preferredStyle: .alert)
+            alertVC.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.navigationController?.present(alertVC, animated: true, completion: nil)
+        } else {
+            // TODO: Update this based on what to do with images
+            images = response!
+        }
+    }
 }
--- VirtualTourist/Model/API Client/APIClient.swift
+++ VirtualTourist/Model/API Client/APIClient.swift
@@ -9,5 +9,66 @@
 import Foundation
 
 class APIClient {
+    // MARK: Struct for authentication and user data
+    struct Auth {
+        static var apiKey = ""
+    }
     
+    // MARK: API Endpoints
+    enum Endpoints {
+        static let base = "https://www.flickr.com/services/rest/"
+        static let searchPhotosMethod = "flickr.photos.search"
+    }
+    
+    // MARK: General JSON response handling
+    // Handle responses or errors for any request type
+    class func handleResponseOrError<ResponseType: Decodable>(_ data: Data?, _ response: URLResponse?, _ error: Error?, _ completion: @escaping (ResponseType?, Error?) -> Void) {
+        guard let data = data else {
+            DispatchQueue.main.async {
+                completion(nil, error)
+            }
+            return
+        }
+        
+        let decoder = JSONDecoder()
+        
+        do {
+            let responseObject = try decoder.decode(ResponseType.self, from: data)
+            DispatchQueue.main.async {
+                completion(responseObject, nil)
+            }
+        } catch {
+            do {
+                let errorResponse = try decoder.decode(ErrorResponse.self, from: data)
+                DispatchQueue.main.async {
+                    completion(nil, errorResponse)
+                }
+            } catch {
+                DispatchQueue.main.async {
+                    completion(nil, error)
+                }
+            }
+        }
+    }
+    
+    // MARK: General Request Types
+    // Send GET Requests
+    class func taskForGETRequest<ResponseType: Decodable>(url: URL, responseType: ResponseType.Type, completion: @escaping (ResponseType?, Error?) -> Void) {
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            handleResponseOrError(data, response, error, completion)
+        }
+        task.resume()
+    }
+    
+    // MARK: Specific API requests
+    class func getPhotosByLocations(latitude: Double, longitude: Double, completion: @escaping (LocationAlbum?, Error?) -> Void) {
+        let url = URL(string: "\(Endpoints.base)?method=\(Endpoints.searchPhotosMethod)&api_key=\(Auth.apiKey)&accuracy=11&lat=\(latitude)&lon=\(longitude)&format=json&nojsoncallback=1")
+        taskForGETRequest(url: url!, responseType: PhotoSearchResponse.self) { response, error in
+            if let response = response {
+                completion(response.photos, nil)
+            } else {
+                completion(nil, error)
+            }
+        }
+    }
 }
