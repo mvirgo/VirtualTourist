062a683bec0a5f74d3c74d3a6f3b651c7fe3b3ad
mvirgo <mvirgs44@gmail.com>
2020-06-28 21:05:41 -0700

    feat: basic logic to create photo urls if none with pin

--- VirtualTourist/Controller/PhotoAlbumViewController.swift
+++ VirtualTourist/Controller/PhotoAlbumViewController.swift
@@ -20,12 +20,24 @@ class PhotoAlbumViewController: UIViewController, MKMapViewDelegate {
     var dataController: DataController!
     var loadedMap: Map!
     var selectedPin: Pin!
-    // TODO: Switch below to actually store images
-    var images: LocationAlbum!
+    var imagesDownload: LocationAlbum!
+    var savedPhotos = [Photo]()
     
     // MARK: View Functions
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
+        setMap()
+        if selectedPin.photos?.count == 0 {
+            // Get photos at the current location
+            getPhotosFromFlickr()
+        } else {
+            // TODO: Get photos from Core Data
+        }
+        // TODO: Show photos in collection view
+    }
+    
+    // MARK: Map View Functions
+    func setMap() {
         mapView.delegate = self
         // Carry over map details
         let pinForMap = MKPointAnnotation()
@@ -33,13 +45,8 @@ class PhotoAlbumViewController: UIViewController, MKMapViewDelegate {
         self.mapView.addAnnotation(pinForMap)
         // Set region of mini-map, dividing latitude span by 4 to be closer to original zoom
         self.mapView.region = MKCoordinateRegion(center: pinForMap.coordinate, span: MKCoordinateSpan(latitudeDelta: loadedMap.spanLatitude / 4, longitudeDelta: loadedMap.spanLongitude))
-        // TODO: Change below logic if images in location already obtained
-        // Get photos at the current location
-        APIClient.getPhotosByLocations(latitude: selectedPin.latitude, longitude: selectedPin.longitude, completion: handlePhotoData(response:error:))
-        // TODO: Go load the images themselves
     }
     
-    // MARK: Map View Functions
     // Below function based on Udacity PinSample code - pin style
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
         
@@ -58,6 +65,11 @@ class PhotoAlbumViewController: UIViewController, MKMapViewDelegate {
         return pinView
     }
     
+    // MARK: API Calls
+    func getPhotosFromFlickr() {
+        APIClient.getPhotosByLocations(latitude: selectedPin.latitude, longitude: selectedPin.longitude, completion: handlePhotoData(response:error:))
+    }
+    
     // MARK: Completion Handlers
     func handlePhotoData(response: LocationAlbum?, error: Error?) {
         if let _ = error {
@@ -66,15 +78,22 @@ class PhotoAlbumViewController: UIViewController, MKMapViewDelegate {
             self.navigationController?.present(alertVC, animated: true, completion: nil)
         } else {
             // TODO: Update this based on what to do with images
-            images = response!
-            if images.photo.count == 0 {
+            imagesDownload = response!
+            if imagesDownload.photo.count == 0 {
                 noImagesLabel.isHidden = false
+            }
+            // Load the images themselves
+            for photoData in imagesDownload.photo {
+                let photoURL = "https://farm\(photoData.farm).staticflickr.com/\(photoData.server)/\(photoData.id)_\(photoData.secret).jpg"
+                // TODO: Get the actual photo
+                // TODO: Store the photo in Core Data
+                // TODO: Add to savedPhotos or show in collection view?
             }
         }
     }
     
     // MARK: IBActions
     @IBAction func newCollectionButtonPressed(_ sender: Any) {
-        // TODO: Implement logic to load a new collection
+        // TODO: Implement logic to load a new collection, and delete old one
     }
 }
